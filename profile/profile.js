ba1965359ec1eee55b2578f72fc8863d2bd707d9
agn <pineapplerind.info@gmail.com>
2022-03-04 22:16:08 -0500

    finish up data structure for User

--- profile/profile.js
+++ profile/profile.js
@@ -1,14 +1,40 @@
-let query = document.location.search.replace('?','')
+let query = document.location.search.replace('?', '')
 document.querySelector('title').innerHTML = query
-const user = {
-    name: {
-        text: '',
-        id: ''
-    }, stats: {
-        likes: 0,
-        views: 0
+const Users = {
+    fetch: function (id) {
+        return this.data.findIndex(obj=>{
+            return obj.name.id === id
+        })
     },
-    following: [],
-    followers: [],
-    password: '' // Hash function (SHA-256, for security)
+    at: function(ind) {return this.data[ind]},
+    remove: function(ind) {
+        return this.data.splice(ind,1)
+    },
+    data: [{
+        name: {
+            text: 'flipanim',
+            id: 'ADMIN'
+        }, stats: {
+            likes: function () { },
+            views: function () { },
+        }, anims: [
+            {
+                name: 'Welcome to New FlipAnim',
+                id: 'FIRSTANIM',
+                stats: {
+                    likes: 1,
+                    views: 2
+                },
+                author: 'ADMIN'
+            }
+        ],
+        status: {
+            name: "Support has arrived!",
+            type: 1 // 0 = invisible, 1 = online, 2 = idle, 3 = dnd
+        },
+        following: [],
+        followers: [],
+        password: '' // Hash function (SHA-256, for security)
+    }]
 }
+console.log(users[0].anims[0].author)
