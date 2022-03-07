fe97561b16817fd9655bfb8cd3117ad03c780593
agn <pineapplerind.info@gmail.com>
2022-03-07 15:47:18 -0500

    Description
    - Move api files to own folder
    - Add Passport library
    - Login endpoint!!!
    - sha256 is its own file

--- api/index.js
+++ api/index.js
@@ -1,80 +1,16 @@
-const fs = require("fs");
+const fs = require("fs")
+const sha256 = require('./sha256')
+
 let got, FormData
 import('got').then(data => got = data.got), import('formdata-node').then(data => FormData = data.FormData)
+const passport = require('passport');
 
 setTimeout(function () { console.log(got, FormData) }, 2000)
 /***********
  * Models       
  **********/
-var Anim = require('./models/Anim'),
-  User = require('./models/User')
-
-var sha256 = function r(t) {
-  function n(r, t) {
-    return (r >>> t) | (r << (32 - t));
-  }
-  for (
-    var o,
-    e,
-    f = Math.pow,
-    h = f(2, 32),
-    a = "",
-    l = [],
-    g = 8 * t.length,
-    c = (r.h = r.h || []),
-    i = (r.k = r.k || []),
-    u = i.length,
-    v = {},
-    s = 2;
-    u < 64;
-    s++
-  )
-    if (!v[s]) {
-      for (o = 0; o < 313; o += s) v[o] = s;
-      (c[u] = (f(s, 0.5) * h) | 0), (i[u++] = (f(s, 1 / 3) * h) | 0);
-    }
-  for (t += ""; (t.length % 64) - 56;) t += "\0";
-  for (o = 0; o < t.length; o++) {
-    if ((e = t.charCodeAt(o)) >> 8) return;
-    l[o >> 2] |= e << (((3 - o) % 4) * 8);
-  }
-  for (l[l.length] = (g / h) | 0, l[l.length] = g, e = 0; e < l.length;) {
-    var k = l.slice(e, (e += 16)),
-      d = c;
-    for (c = c.slice(0, 8), o = 0; o < 64; o++) {
-      var p = k[o - 15],
-        w = k[o - 2],
-        A = c[0],
-        C = c[4],
-        M =
-          c[7] +
-          (n(C, 6) ^ n(C, 11) ^ n(C, 25)) +
-          ((C & c[5]) ^ (~C & c[6])) +
-          i[o] +
-          (k[o] =
-            o < 16
-              ? k[o]
-              : (k[o - 16] +
-                (n(p, 7) ^ n(p, 18) ^ (p >>> 3)) +
-                k[o - 7] +
-                (n(w, 17) ^ n(w, 19) ^ (w >>> 10))) |
-              0);
-      (c = [
-        (M +
-          ((n(A, 2) ^ n(A, 13) ^ n(A, 22)) +
-            ((A & c[1]) ^ (A & c[2]) ^ (c[1] & c[2])))) |
-        0,
-      ].concat(c))[4] = (c[4] + M) | 0;
-    }
-    for (o = 0; o < 8; o++) c[o] = (c[o] + d[o]) | 0;
-  }
-  for (o = 0; o < 8; o++)
-    for (e = 3; e + 1; e--) {
-      var S = (c[o] >> (8 * e)) & 255;
-      a += (S < 16 ? 0 : "") + S.toString(16);
-    }
-  return a;
-};
+var Anim = require('../models/Anim'),
+  User = require('../models/User')
 
 module.exports = {
   showComments: function (req, res) {
@@ -218,40 +154,6 @@ module.exports = {
         })
       })
     },
-  }, login: function (req, res) {
-    let username = req.body.username,
-      password = req.body.password;
-    console.log(req.body);
-    if (username && password) {
-      password = sha256(password);
-      let file = JSON.parse(fs.readFileSync("data/users.json", "utf-8"));
-      let user =
-        file[
-        file.findIndex((obj) => {
-          return obj.name.text.toLowerCase() === username.toLowerCase();
-        })
-        ];
-      if (!user) return incorrect();
-      if (sha256(password) === sha256(user.password)) {
-        res.status(200);
-        res.send("200 OK");
-        console.log(req.session)
-        req.session.loggedin = true;
-        req.session.username = username
-        console.log(req.session)
-        return;
-      } else return incorrect();
-    } else {
-      res.status(400);
-      res.send("400 Bad Request: No Username or no Password");
-      return;
-    }
-
-    function incorrect() {
-      res.status(401);
-      res.send("401 Username or Password Incorrect");
-      return;
-    }
   }, getAnimComments: function (req, res) {
     console.log(req.session, req.sessionID)
     let animId = req.path.slice(req.path.indexOf('anims/') + 'anims/'.length).replace('/comments', '')
@@ -265,5 +167,16 @@ module.exports = {
       res.send("404 Anim Not Found");
     } else res.send(file[anim].comments)
     return
-  }
+  }, login: async function (req, res, next) {
+    // configure passport.js to use the local strategy
+    passport.authenticate('local', (err, user, info) => {
+      if (info) { return res.send(info.message) }
+      if (err) { return next(err); }
+      if (!user) { return res.redirect('/login'); }
+      req.login(user, (err) => {
+        return res.send('You were authenticated & logged in!\n');
+      })
+    })(req, res, next);
+  },// request.isAuthenticated() is the function to determine
+  //authentication
 }
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var express = require("express"),
     session = require("express-session"),
-    FileStore = require('session-file-store')(session),
     app = express(),
     cors = require("cors"),
     bodyParser = require("body-parser"),
     mongoose = require('mongoose'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    FileStore = require('session-file-store')(session),
     server = app.listen(process.env.PORT || 3000, listen),
-    router = express.Router();
-var api = require('./api.js');
+    router = express.Router()
+const res = require("express/lib/response");
+var api = require('./api/index.js'),
+    User = require('./models/User'),
+    sha256 = require('./api/sha256')
 
 const dbUrl = 'mongodb+srv://root:flipanimapipass@flipanim.z85ki.mongodb.net/flipanim?retryWrites=true&w=majority'
 
@@ -19,7 +24,6 @@ mongoose.connect(dbUrl, {
 }, (err) => {
     if (err) console.log(err);
 });
-
 
 function listen() {
     return console.log('Server is listening');
@@ -37,6 +41,9 @@ app.use(
         saveUninitialized: true,
     })
 );
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(
@@ -44,6 +51,48 @@ app.use(
         extended: true,
     })
 );
+
+passport.use(new LocalStrategy(
+    { usernameField: 'username' },
+    async (username, password, done,) => {
+        console.log('Inside local strategy callback')
+        // here is where you make a call to the database
+        // to find the user based on their username or email address
+        // for now, we'll just pretend we found that it was users[0]
+        await User.findOne({
+            username: username
+        }).then(user => {
+            if (username === user.name.text && sha256(password) === user.password) {
+                console.log('Local strategy returned true')
+                return done(null, user)
+            } else {
+                console.log('Incorrect')
+                return done(null, false, { message: 'Invalid credentials.\n' })
+            }
+        }).catch(err => {
+            console.error(err)
+        })
+
+    }
+));
+
+// tell passport how to serialize the user
+passport.serializeUser((user, done) => {
+    console.log('Inside serializeUser callback. User id is save to the session file store here')
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    console.log('Inside deserializeUser callback')
+    console.log(`The user id passport saved in the session file store is: ${id}`)
+    await User.findOne({
+        id: id
+    }).then(user => {
+        done(null, user);
+    }).catch(err => {
+        console.error(err)
+    })
+});
 
 app.use(express.static("public")); // Page itself
 app.route("/api/v1/users").get(api.showUser); // For individual user requests!
