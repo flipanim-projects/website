e1d16d87b121e0a2ea5127f3d2f13c13d0d7c11c
agn <pineapplerind.info@gmail.com>
2022-03-09 19:17:09 -0500

    nuke react into orbit
    this is nice

--- src/pages/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as Settings } from "./Settings"
-export { default as Follows } from "./Follows";
-export { default as Profile } from "./Profile";
-export { default as Browse } from "./Browse";
