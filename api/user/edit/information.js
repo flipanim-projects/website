524cbeeadcb5c9af8869ab9317878591772f00dc
northernlights03 <65303441+northernlights03@users.noreply.github.com>
2022-03-30 15:14:14 -0400

    hopefully don't touch immutable properties

--- api/user/edit/information.js
+++ api/user/edit/information.js
@@ -1,43 +1,62 @@
-const User = require('../../../models/User'),
-    CaptchaHandler = require('../../utils/captcha')
+const User = require("../../../models/User"),
+  CaptchaHandler = require("../../utils/captcha");
 async function information(req, res) {
-    if (!req.body['displayName'] && !req.body['bio']) return res.status(400).json({
+  if (!req.body["displayName"] && !req.body["bio"])
+    return res.status(400).json({
+      status: 400,
+      message: "400 Bad Request: No data provided",
+    });
+
+  let hcaptcha = req.body["h-captcha-response"];
+  new CaptchaHandler().send({
+    hcaptcha: hcaptcha,
+    invalid: function () {
+      res.status(400).json({
         status: 400,
-        message: '400 Bad Request: No data provided'
-    })
+        message: "400 Bad Request: Invalid Captcha",
+      });
+    },
+    next: function () {
+      editUser();
+    },
+  });
 
-    let hcaptcha = req.body['h-captcha-response']
-    new CaptchaHandler().send({
-        hcaptcha: hcaptcha,
-        invalid: function () {
-            res.status(400).json({
-                status: 400,
-                message: '400 Bad Request: Invalid Captcha'
-            })
-        }, next: function () {
-            editUser()
+  let toupdate = req.user;
+  function removeProp(obj, propToDelete) {
+    for (var property in obj) {
+      if (obj.hasOwnProperty(property)) {
+        if (typeof obj[property] == "object") {
+          return removeProp(obj[property], propToDelete);
+        } else {
+          if (property === propToDelete && obj[property] === true) {
+            delete obj[property];
+          }
         }
-    })
-
-    let toupdate = req.user
-
-    async function editUser() {
-        req.body['bio'] ? toupdate.bio = req.body['bio'] : null
-        if (req.body['displayName']) toupdate.name.display = req.body['displayName']
-        if (req.body['theme']) toupdate.preferences.theme = req.body['theme']
-        await User.findByIdAndUpdate(req.session.passport.user, toupdate).then(() => {
-            res.status(200).json({
-                status: 200,
-                message: "200 OK User Updated"
-            })
-        }).catch(err => {
-            console.log(err)
-            return res.status(500).json({
-                status: 500,
-                message: "Internal Server Error"
-            })
-        })
+      }
     }
+  }
+  removeProp(toupdate, "_id");
+  delete toupdate._v;
 
+  async function editUser() {
+    req.body["bio"] ? (toupdate.bio = req.body["bio"]) : null;
+    if (req.body["displayName"])
+      toupdate.name.display = req.body["displayName"];
+    if (req.body["theme"]) toupdate.preferences.theme = req.body["theme"];
+    await User.findByIdAndUpdate(req.session.passport.user, toupdate)
+      .then(() => {
+        res.status(200).json({
+          status: 200,
+          message: "200 OK User Updated",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json({
+          status: 500,
+          message: "Internal Server Error",
+        });
+      });
+  }
 }
-module.exports = information
+module.exports = information;
